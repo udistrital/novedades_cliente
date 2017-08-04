dc49bf3230c7d6c0dfe0470319cc052dde245c24
Camilo Torres <camilo@ulises.udistritaloas.edu.co>
2017-08-04 15:14:34 -0500

    Se cambia el servicio de administrativa ya que apuntaba a local

--- app/scripts/services/administrativa_service.js
+++ app/scripts/services/administrativa_service.js
@@ -14,8 +14,8 @@ angular.module('administrativaService',[])
     //var path = "http://10.20.2.15:8081/v1/";
     //var path = "http://10.20.2.78:8090/v1/";
 
-    /*var path = "http://10.20.0.254/administrativa_api/v1/";*/
-    var path = "http://localhost:8081/v1/";
+    var path = "http://10.20.0.254/administrativa_api/v1/";
+    //var path = "http://localhost:8082/v1/";
     // Public API here
     return {
       get: function (tabla,params) {
