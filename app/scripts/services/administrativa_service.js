eac5b082c951dfaa0a333c81719b9bd7c57c2d3b
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2018-01-02 11:20:32 -0500

    merge para servicios de administrativa crud

--- app/scripts/services/administrativa_service.js
+++ app/scripts/services/administrativa_service.js
@@ -13,12 +13,9 @@
     // Service logic
     // ...
     var path = "http://10.20.0.254/administrativa_api/v1/";
-<<<<<<< HEAD
     //var path = "http://localhost:8080/v1/";
-=======
     //var path = "http://10.20.2.150:8082/v1/";
-    //var path = "http://10.20.0.138:8090/v1/";  
->>>>>>> 8ab24e7c71bab2054d08ee75673c0e067695ee0b
+    //var path = "http://10.20.0.138:8090/v1/";
     // Public API here
     return {
       get: function (tabla,params) {
