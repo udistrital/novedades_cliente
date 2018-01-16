a854706e9c1303021b0bed72c9fe8cb9b142b5f9
Kmilo9433 <camilo@ulises.udistritaloas.edu.co>
2018-01-16 17:34:01 -0500

    Se modifica modal

--- app/scripts/services/amazon_administrativa.js
+++ app/scripts/services/amazon_administrativa.js
@@ -12,8 +12,8 @@
       // Service logic
       // ...
 
-      //var path = "http://10.20.0.254/administrativa_amazon_api/v1/";
-      var path = "http://localhost:8084/v1/";
+      var path = "http://10.20.0.254/administrativa_amazon_api/v1/";
+      //var path = "http://localhost:8084/v1/";
       // Public API here
       return {
         get: function (tabla,params) {
