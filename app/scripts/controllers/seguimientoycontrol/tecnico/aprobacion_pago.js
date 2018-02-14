7930af2abc80e7cf27da1f17ef8d16485845e88a
Javier Reyes <ingjsreyesm@gmail.com>
2018-02-13 20:04:45 -0500

    Se genera certificado hacia Recursos Humanos

--- app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_pago.js
+++ app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_pago.js
@@ -8,7 +8,7 @@
  * Controller of the contractualClienteApp
  */
 angular.module('contractualClienteApp')
-  .controller('AprobacionPagoCtrl', function (oikosRequest, $http, uiGridConstants, contratoRequest, $translate, administrativaRequest,$routeParams) {
+  .controller('AprobacionPagoCtrl', function (oikosRequest, $http, uiGridConstants, contratoRequest, $translate, administrativaRequest,$routeParams,adminMidRequest) {
 
 
       //Variable de template que permite la edición de las filas de acuerdo a la condición ng-if
@@ -19,6 +19,14 @@ angular.module('contractualClienteApp')
       self.Documento = $routeParams.docid;
 
       self.contratistas = [];
+
+      /*
+        Función para obtener la imagen del escudo de la universidad
+      */
+      $http.get("scripts/models/imagen_ud.json")
+       .then(function(response) {
+         self.imagen = response.data;
+      });
 
       /*
         Creación tabla que tendrá todos los contratistas relacionados al supervisor
@@ -128,6 +136,7 @@ angular.module('contractualClienteApp')
         contratoRequest.get('ordenador', self.Documento).then(function (response) {
 
           self.ordenador = response.data;
+          console.log(self.ordenador);
 
           //Petición para obtener el Id de la relación de acuerdo a los campos
           administrativaRequest.get('pago_mensual', $.param({
@@ -299,6 +308,73 @@ angular.module('contractualClienteApp')
           })
         });
 
+      };
+
+      /*
+        Función que genera el documento de quienes no cumplieron con sus obligaciones
+      */
+      self.getContenido = function(){
+        var contenido = [];
+        contenido.push( {text:'EL SUSCRITO DECANO DE LA FACULTAD DE INGENIERÍA DE LA UNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
+        contenido.push({text:'CERTIFICA QUE: ', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
+        contenido.push({text:'De acuerdo con la información suministrada por los proyectos curriculares de pregrado de la facultad de Ingeniería, los profesores de Vinculación Especial contratados para el período académico 2018-I, cumplieron a cabalidad con las funciones docentes en el mes de febrero del presente año. (De acuerdo a calendario académico).', style:'general_font'}, '\n\n')
+        if(self.docentes_pago_rechazado){
+          contenido.push({text:'A excepción de las siguientes novedades: ', style:'general_font'}, '\n')
+          angular.forEach(self.docentes_pago_rechazado, function(value) {
+           contenido.push({text: value.Nombre + ', ' + value.NumDocumento, style:'general_font'});
+         });
+        }
+        contenido.push('\n',{text:'La presente certificación se expide con destino a la División de Recursos Humanos a los catorce días del mes de febrero de 2018.',  style:'general_font'}, '\n\n\n\n\n\n');
+        contenido.push({text:'' + self.ordenador.nombre, style:'bottom_space'});
+        contenido.push({text:'' + self.ordenador.cargo, style:'bottom_space'});
+        return contenido
+      }
+
+      /*
+        Función que genera el documento de quienes no cumplieron con sus obligaciones
+      */
+      self.generarPDF = function (){
+        //adminMidRequest.get('aprobacion_pago/certificacion_visto_bueno/*/**/*').
+        adminMidRequest.get('aprobacion_pago/certificacion_documentos_aprobados/14/2/2018').
+          then(function(response){
+            self.docentes_pago_rechazado = response.data;
+            console.log(self.docentes_pago_rechazado);
+
+            //Generación documento
+            var docDefinition = {
+              pageMargins: [30, 140, 40, 40],
+              header: {
+               height: 120,
+               width: 120,
+               image: self.imagen.imagen,
+               margin: [100, 15,5,5],
+               alignment: 'center'
+             },
+             content: self.getContenido(),
+             styles: {
+               top_space: {
+                 fontSize: 11,
+                 marginTop: 30
+               },
+               bottom_space: {
+                 fontSize: 12,
+                 bold: true,
+                 alignment:'center'
+                 //marginBottom: 30
+               },
+               general_font:{
+                 fontSize: 11,
+                 alignment: 'justify'
+               }
+             }
+            }
+            //Variable para obtener la fecha y hora que se genera el dcoumento
+            var date = new Date();
+            date = moment(date).format('DD_MMM_YYYY_HH_mm_ss');
+
+            //Sirve para descargar el documento y setearle el nombre
+            pdfMake.createPdf(docDefinition).download('Certificación cumplido coordinación ' + date + '.pdf');
+           });
       };
 
   });
