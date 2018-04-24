cdd5a0da42f172966050e31790955a0bb96d13f2
Oscar Ivan Duarte Torres <o.duarte7@gmail.com>
2018-04-24 15:51:45 -0500

    Se ajusta la aprobación masiva

--- app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_pago.js
+++ app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_pago.js
@@ -394,6 +394,8 @@ angular.module('contractualClienteApp')
         cancelButtonText: 'Cancelar',
         confirmButtonText: 'Aceptar'
       }).then(function () {
+        
+         self.solicitudes_seleccionadas = self.gridApi.selection.getSelectedRows();
         adminMidRequest.post('aprobacion_pago/aprobar_pagos',self.solicitudes_seleccionadas).then(function(response){
           if (response.data === 'OK'){
             swal(
