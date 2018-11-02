3ee67e43d3965d77b829ce839a00624bb60ad6b3
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2018-11-02 11:54:18 -0500

    se agrega vista para pdf necesidad

---
+++ app/scripts/controllers/necesidad/necesidad_vista.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contractualClienteApp.controller:NecesidadVistaCtrl
+ * @description
+ * # NecesidadVistaCtrl
+ * Controller of the contractualClienteApp
+ */
+angular.module('contractualClienteApp')
+  .controller('NecesidadVistaCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
