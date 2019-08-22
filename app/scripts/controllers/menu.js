967ac70c763bda0638af6a1cec13fe1d8c55fa67
Brayanb24 <babermudezb@correo.udistrital.edu.co>
2019-08-22 15:12:10 -0500

    Corrigiendo menú de novedades_cliente

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -23,8 +23,7 @@ angular.module('contractualClienteApp')
 
         // optiene los menus segun el rol
         var roles = rolesService.roles().toString().replace(/,/g, '%2C');
-        configuracionRequest.get('menu_opcion_padre/ArbolMenus/' + roles + '/Argo', '').then(function (response) {
-
+        configuracionRequest.get('menu_opcion_padre/ArbolMenus/ADMINISTRADOR_NOVEDADES/Novedades', '').then(function (response) {
             $rootScope.my_menu = response.data;
 
         });
