8d911d4d51bceee8a70abeed5381ae041ee78221
anlramirezs <anlramirezs11@gmail.com>
2020-01-20 11:42:40 -0500

    Se ajusta logica del cliente a la API administrativa_amazon_api para la vista 'Legal'. Issue #110

--- app/scripts/controllers/seguimientoycontrol/legal.js
+++ app/scripts/controllers/seguimientoycontrol/legal.js
@@ -8,7 +8,7 @@
  * Controller of the contractualClienteApp
  */
 angular.module('contractualClienteApp')
-    .controller('SeguimientoycontrolLegalCtrl', function ($scope, amazonAdministrativaRequest, coreAmazonRequest, $translate, contratoRequest, novedadesMidRequest, novedadesRequest) {
+    .controller('SeguimientoycontrolLegalCtrl', function ($scope, coreAmazonRequest, $translate, novedadesMidRequest, novedadesRequest, agoraRequest) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
@@ -22,7 +22,7 @@ angular.module('contractualClienteApp')
         self.vigencia_seleccionada = self.vigencias[0];
         self.contrato_obj = {};
         self.estado_resultado_response = false;
-        amazonAdministrativaRequest.get('vigencia_contrato', '').then(function (response) {
+        agoraRequest.get('vigencia_contrato', '').then(function (response) {
             $scope.vigencias = response.data;
         });
 
@@ -34,21 +34,24 @@ angular.module('contractualClienteApp')
          * funcion para obtener la totalidad de los contratos por vigencia seleccionada
          */
         self.buscar_contrato = function () {
-            contratoRequest.get('contrato', +self.contrato_id + '/' + self.contrato_vigencia).then(function (wso_response) {
-                if (wso_response.data.contrato.numero_contrato_suscrito) {
-                    self.contrato_obj.id = wso_response.data.contrato.numero_contrato_suscrito;
-                    self.contrato_obj.valor = wso_response.data.contrato.valor_contrato;
-                    self.contrato_obj.objeto = wso_response.data.contrato.objeto_contrato;
-                    self.contrato_obj.fecha_registro = wso_response.data.contrato.fecha_registro;
-                    self.contrato_obj.ordenador_gasto_nombre = wso_response.data.contrato.ordenador_gasto.nombre_ordenador;
-                    self.contrato_obj.ordenador_gasto_rol = wso_response.data.contrato.ordenador_gasto.rol_ordenador;
-                    self.contrato_obj.vigencia = wso_response.data.contrato.vigencia;
-                    self.contrato_obj.contratista = wso_response.data.contrato.contratista;
+            agoraRequest.get('contrato_general/?query=ContratoSuscrito.NumeroContratoSuscrito:' + self.contrato_id + ',VigenciaContrato:' + self.contrato_vigencia).then(function (agora_response) {
+                if (agora_response.data.length > 0) {
+                    self.contrato_obj.numero_contrato = self.contrato_id;
+                    self.contrato_obj.id = agora_response.data[0].ContratoSuscrito[0].Id;
+                    self.contrato_obj.valor = agora_response.data[0].ValorContrato;
+                    self.contrato_obj.objeto = agora_response.data[0].ObjetoContrato;
+                    self.contrato_obj.fecha_registro = agora_response.data[0].FechaRegistro;
+                    self.contrato_obj.ordenador_Id = agora_response.data[0].OrdenadorGasto;
+                    self.contrato_obj.vigencia = self.contrato_vigencia;
+                    self.contrato_obj.contratista = agora_response.data[0].Contratista;
                     self.contrato_obj.cesion = 0;
+
+                    console.log(self.contrato_obj)
+
                     //Obtiene el estado del contrato.
-                    contratoRequest.get('contrato_estado', +self.contrato_id + '/' + self.contrato_vigencia).then(function (ce_response) {
-                        self.estado_contrato_obj.estado = ce_response.data.contratoEstado.estado.id;
-                        console.log(self.estado_contrato_obj.estado)
+                    agoraRequest.get('contrato_estado?query=NumeroContrato:' + self.contrato_obj.id + ',Vigencia:' + self.contrato_obj.vigencia).then(function (ce_response) {
+                        self.estado_contrato_obj.estado = ce_response.data[ce_response.data.length - 1].Estado.Id;
+                        console.info(self.estado_contrato_obj.estado)
                         if (self.estado_contrato_obj.estado == 7) {
                             swal(
                                 $translate.instant('CONTRATO_CANCELADO'),
@@ -69,83 +72,53 @@ angular.module('contractualClienteApp')
                                 'info'
                             );
                         }
-                        //Obtiene el tipo de contrato y el tipo de la ultima novedad hecha para saber si el contrato fue cedido.
-                        amazonAdministrativaRequest.get('tipo_contrato?query=Id:' + wso_response.data.contrato.tipo_contrato).then(function (tc_response) {
-                            self.contrato_obj.tipo_contrato = tc_response.data[0].TipoContrato;
-                            novedadesMidRequest.get('novedad', self.contrato_obj.id + "/" + self.contrato_obj.vigencia).then(function (response_sql) {
-                                var elementos_cesion = response_sql.data.Body;
-                                if (elementos_cesion.length != '0') {
-                                    var last_newness = elementos_cesion[elementos_cesion.length - 1];
-                                    novedadesRequest.get('tipo_novedad', 'query=Id:' + last_newness.tiponovedad).then(function (nr_response) {
-                                        self.contrato_obj.tipo_novedad = nr_response.data[0].CodigoAbreviacion;
-                                        if (self.contrato_obj.tipo_novedad == "NP_CES") {
-                                            self.contrato_obj.contratista = last_newness.cesionario;
-                                            if(last_newness.poliza===""){
-                                                self.estado_contrato_obj.estado=1
-                                                swal(
-                                                    $translate.instant('INFORMACION'),
-                                                    $translate.instant('DESCRIPCION_ACTA_CESION'),
-                                                    'info'
-                                                );
-                                            }
-                                        } else if (self.contrato_obj.tipo_novedad == "NP_SUS" || self.contrato_obj.tipo_novedad == "NP_REI"
-                                            || self.contrato_obj.tipo_novedad == "NP_ADI" || self.contrato_obj.tipo_novedad == "NP_PRO"
-                                            || self.contrato_obj.tipo_novedad == "NP_ADPRO") {
-                                            self.contrato_obj.contratista = last_newness.cesionario;
-                                        }
-                                        //Obtiene los datos aosicados al proveedor de un contrato que ha tenido una novedad
-                                        amazonAdministrativaRequest.get('informacion_proveedor?query=Id:' + self.contrato_obj.contratista).then(function (ip_response) {
-                                            self.contrato_obj.contratista_documento = ip_response.data[0].NumDocumento;
-                                            self.contrato_obj.contratista_nombre = ip_response.data[0].NomProveedor;
-                                            if (ip_response.data[0].Tipopersona == 'NATURAL') {
-                                                amazonAdministrativaRequest.get('informacion_persona_natural?query=Id:' + self.contrato_obj.contratista_documento).then(function (ipn_response) {
-                                                    coreAmazonRequest.get('ciudad', 'query=Id:' + ipn_response.data[0].IdCiudadExpedicionDocumento).then(function (c_response) {
-                                                        self.contrato_obj.contratista_ciudad_documento = c_response.data[0].Nombre;
-                                                        self.estado_resultado_response = true;
-                                                    });
-                                                });
-                                            }
-                                            else {
-                                                amazonAdministrativaRequest.get('informacion_persona_juridica?query=Id:' + ip_response.data[0].NumDocumento).then(function (ipn_response) {
-                                                    amazonAdministrativaRequest.get('informacion_proveedor', 'query=num_documento:' + ip_response.data[0].NumDocumento).then(function (ip_response) {
-                                                        coreAmazonRequest.get('ciudad', 'query=Id:' + ip_response.data[0].IdCiudadContacto).then(function (c_response) {
-                                                            self.contrato_obj.contratista_ciudad_documento = c_response.data[0].Nombre;
-                                                            self.estado_resultado_response = true;
-                                                        });
-                                                    });
-                                                });
-                                            }
-                                        });
 
-                                    });
-                                } else {
-                                    //Obtiene los datos aosicados al proveedor de un contrato que no tiene novedades
-                                    amazonAdministrativaRequest.get('informacion_proveedor?query=Id:' + self.contrato_obj.contratista).then(function (ip_response) {
+                        //Obtiene el tipo de contrato y el tipo de la ultima novedad hecha para saber si el contrato fue cedido.
+                        novedadesMidRequest.get('novedad', self.contrato_obj.numero_contrato + "/" + self.contrato_obj.vigencia).then(function (response_sql) {
+                            var elementos_cesion = response_sql.data.Body;
+                            if (elementos_cesion.length != '0') {
+                                var last_newness = elementos_cesion[elementos_cesion.length - 1];
+                                novedadesRequest.get('tipo_novedad', 'query=Id:' + last_newness.tiponovedad).then(function (nr_response) {
+                                    self.contrato_obj.tipo_novedad = nr_response.data[0].CodigoAbreviacion;
+                                    if (self.contrato_obj.tipo_novedad == "NP_CES") {
+                                        self.contrato_obj.contratista = last_newness.cesionario;
+                                        if (last_newness.poliza === "") {
+                                            self.estado_contrato_obj.estado = 1
+                                            swal(
+                                                $translate.instant('INFORMACION'),
+                                                $translate.instant('DESCRIPCION_ACTA_CESION'),
+                                                'info'
+                                            );
+                                        }
+                                    } else if (self.contrato_obj.tipo_novedad == "NP_SUS" || self.contrato_obj.tipo_novedad == "NP_REI"
+                                        || self.contrato_obj.tipo_novedad == "NP_ADI" || self.contrato_obj.tipo_novedad == "NP_PRO"
+                                        || self.contrato_obj.tipo_novedad == "NP_ADPRO") {
+                                        self.contrato_obj.contratista = last_newness.cesionario;
+                                    }
+                                    //Obtiene los datos aosicados al proveedor de un contrato que ha tenido una novedad
+                                    agoraRequest.get('informacion_proveedor?query=Id:' + self.contrato_obj.contratista).then(function (ip_response) {
                                         self.contrato_obj.contratista_documento = ip_response.data[0].NumDocumento;
                                         self.contrato_obj.contratista_nombre = ip_response.data[0].NomProveedor;
-                                        if (ip_response.data[0].Tipopersona == 'NATURAL') {
-                                            amazonAdministrativaRequest.get('informacion_persona_natural?query=Id:' + self.contrato_obj.contratista_documento).then(function (ipn_response) {
-                                                coreAmazonRequest.get('ciudad', 'query=Id:' + ipn_response.data[0].IdCiudadExpedicionDocumento).then(function (c_response) {
-                                                    self.contrato_obj.contratista_ciudad_documento = c_response.data[0].Nombre;
-                                                    self.estado_resultado_response = true;
-                                                });
-                                            });
-                                        }
-                                        else {
-                                            amazonAdministrativaRequest.get('informacion_persona_juridica?query=Id:' + ip_response.data[0].NumDocumento).then(function (ipn_response) {
-                                                amazonAdministrativaRequest.get('informacion_proveedor', 'query=num_documento:' + ip_response.data[0].NumDocumento).then(function (ip_response) {
-                                                    coreAmazonRequest.get('ciudad', 'query=Id:' + ip_response.data[0].IdCiudadContacto).then(function (c_response) {
-                                                        self.contrato_obj.contratista_ciudad_documento = c_response.data[0].Nombre;
-                                                        self.estado_resultado_response = true;
-                                                    });
-                                                });
-                                            });
-                                        }
+                                        self.estado_resultado_response = true;
                                     });
-                                }
 
-                            });
+                                });
+                            } else {
+                                //Obtiene los datos aosicados al proveedor de un contrato que no tiene novedades
+                                agoraRequest.get('informacion_proveedor?query=Id:' + self.contrato_obj.contratista).then(function (ip_response) {
+                                    self.contrato_obj.contratista_documento = ip_response.data[0].NumDocumento;
+                                    self.contrato_obj.contratista_nombre = ip_response.data[0].NomProveedor;
+                                    self.estado_resultado_response = true;
+                                });
+                            }
                         });
+
+                    }).catch(function (error) {
+                        swal(
+                            $translate.instant('INFORMACION'),
+                            $translate.instant('No se pudo obtener datos del estado del contrato o no hay registros asociados en base de datos a este contrato'),
+                            'info'
+                        )
                     });
                 } else {
                     self.estado_resultado_response = false;
@@ -155,6 +128,7 @@ angular.module('contractualClienteApp')
                         'error'
                     );
                 }
+
             });
         }
 
@@ -178,21 +152,5 @@ angular.module('contractualClienteApp')
                 { field: 'informacion_proveedor.NomProveedor', displayName: $translate.instant('NOMBRE_CONTRATISTA'), width: 390 },
                 { field: 'contrato.valor_contrato', displayName: $translate.instant('VALOR'), cellFilter: 'currency', width: 180 }
             ],
-            onRegisterApi: function (gridApi) {
-                self.gridApi = gridApi;
-                gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-                    self.row_c = row.entity;
-                    self.estado_resultado_response = 0;
-                    contratoRequest.get('contrato_estado', +self.row_c.contrato.numero_contrato_suscrito + '/' + self.row_c.contrato.vigencia).then(function (response) {
-                        var estado = response.data.contratoEstado.estado;
-                        if (estado.id != 8) {
-                            self.estado_contrato_obj.estado = estado.id;
-                            self.estado_resultado_response = response.status;
-                        } else {
-                            self.estado_resultado_response = 0;
-                        }
-                    });
-                });
-            }
         };
     });
--- app/scripts/services/agora_service.js
+++ app/scripts/services/agora_service.js
@@ -8,23 +8,20 @@
  * Factory in the contractualClienteApp.
  */
 angular.module('agoraService', [])
-    .factory('agoraRequest', function($http, CONF, token_service) {
-        // Service logic
-        // ...
-        //var path = "http://10.20.0.254/administrativa_amazon_api/v1/";
+    .factory('agoraRequest', function ($http, CONF, token_service) {
         var path = CONF.GENERAL.ADMINISTRATIVA_PRUEBAS_SERVICE;
         // Public API here
         return {
-            get: function(tabla, params) {
-                return $http.get(path + tabla + "/?" + params, token_service.setting_bearer.headers);
+            get: function(tabla) {
+                return $http.get(path + tabla, token_service.setting_bearer.headers);
             },
-            post: function(tabla, elemento) {
+            post: function (tabla, elemento) {
                 return $http.post(path + tabla, elemento, token_service.setting_bearer.headers);
             },
-            put: function(tabla, id, elemento) {
+            put: function (tabla, id, elemento) {
                 return $http.put(path + tabla + "/" + id, elemento, token_service.setting_bearer.headers);
             },
-            delete: function(tabla, id) {
+            delete: function (tabla, id) {
                 return $http.delete(path + tabla + "/" + id, token_service.setting_bearer.headers);
             }
         };
