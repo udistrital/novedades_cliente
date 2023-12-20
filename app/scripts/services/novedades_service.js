520f9b1ce5435f0b0a9260854d70259cefdcd9f2
Yaov05 <ortizalejandro551199@gmail.com>
2023-12-20 08:45:52 -0500

    fix: cambio estado registro tras error en replica

--- app/scripts/services/novedades_service.js
+++ app/scripts/services/novedades_service.js
@@ -8,21 +8,25 @@
  * Factory in the novedadesService
  */
 angular.module('novedadesService', [])
-    .factory('novedadesRequest', function($http, token_service, CONF) {
+    .factory('novedadesRequest', function ($http, token_service, CONF) {
         // Service logic
         // ...
         var path = CONF.GENERAL.NOVEDADES_SERVICE;
         return {
-            get: function(tabla, params) {
-                return $http.get(path + tabla + "/?" + params, token_service.setting_bearer.headers);
+            get: function (tabla, params) {
+                if (angular.isUndefined(params)) {
+                    return $http.get(path + tabla, token_service.setting_bearer.headers);
+                } else {
+                    return $http.get(path + tabla + "/?" + params, token_service.setting_bearer.headers);
+                }
             },
-            post: function(tabla, elemento) {
+            post: function (tabla, elemento) {
                 return $http.post(path + tabla, elemento, token_service.setting_bearer.headers);
             },
-            put: function(tabla, id, elemento) {
+            put: function (tabla, id, elemento) {
                 return $http.put(path + tabla + "/" + id, elemento, token_service.setting_bearer.headers);
             },
-            delete: function(tabla, id) {
+            delete: function (tabla, id) {
                 return $http.delete(path + tabla + "/" + id, token_service.setting_bearer.headers);
             }
         };
