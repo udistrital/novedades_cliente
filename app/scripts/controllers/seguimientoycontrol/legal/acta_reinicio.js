247112e3bc79fd13a59e0f4d17379b486062a5bb
Yaov05 <ortizalejandro551199@gmail.com>
2024-07-19 12:16:54 -0500

    fix: plazo de ejecución suspensión

--- app/scripts/controllers/seguimientoycontrol/legal/acta_reinicio.js
+++ app/scripts/controllers/seguimientoycontrol/legal/acta_reinicio.js
@@ -1161,7 +1161,7 @@ angular
                                     style: "topHeader",
                                 },
                                 {
-                                    text: self.suspension_obj.PlazoEjecucion + " Meses",
+                                    text: self.suspension_obj.PlazoEjecucion + " Días",
                                     style: "topHeader",
                                 },
                                 ],
