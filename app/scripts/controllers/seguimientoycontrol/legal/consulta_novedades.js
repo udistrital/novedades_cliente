46ad7bd5383e9d7d106716aa627ad7a9d82f9dd9
cristhianLagunaM <claguna201@gmail.com>
2025-09-05 15:38:42 -0500

    feat/anular-novedades

--- app/scripts/controllers/seguimientoycontrol/legal/consulta_novedades.js
+++ app/scripts/controllers/seguimientoycontrol/legal/consulta_novedades.js
@@ -1,21 +1,33 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name contractualClienteApp.controller:SeguimientoycontrolLegalConsultaNovedadesCtrl
- * @description
- * # SeguimientoycontrolLegalConsultaNovedadesCtrl
- * Controller of the contractualClienteApp
- */
 angular.module('contractualClienteApp')
-  .controller('SeguimientoycontrolLegalConsultaNovedadesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+  .controller('SeguimientoycontrolLegalConsultaNovedadesCtrl', function ($scope, novedadesMidRequest, $log) {
     var self = this;
+
+    self.anularFila = function (entity) {
+      var id = entity.Id || entity.id || entity.IdNovedad || entity.novedadId;
+      if (!id) {
+        $log.error('No se encontró Id en la fila:', entity);
+        if (window.swal) swal('Error', 'No se encontró el Id de la novedad en la fila', 'error');
+        return;
+      }
+      var payload = { Activo: false };
+
+      $log.info('PATCH /v1/novedad/' + id, payload);
+      novedadesMidRequest.patch('novedad', id, payload)
+        .then(function () {
+          if (window.swal) swal('Éxito', 'Novedad ' + id + ' anulada', 'success');
+          if (Object.prototype.hasOwnProperty.call(entity, 'Activo')) entity.Activo = false;
+          if (Object.prototype.hasOwnProperty.call(entity, 'estado')) entity.estado = 'ANULADA';
+        })
+        .catch(function (err) {
+          $log.error('Error PATCH /v1/novedad/' + id, err);
+          if (window.swal) swal('Error', 'No fue posible anular la novedad ' + id, 'error');
+        });
+    };
+
+    $scope.anularFila = self.anularFila;
+
     self.gridOptions = {
       enableFiltering: true,
       enableSorting: true,
@@ -23,7 +35,8 @@ angular.module('contractualClienteApp')
       multiSelect: false,
       enableSelectAll: false,
       columnDefs: [
-        { field: 'row', displayName: 'No', width: 50, cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row)+1}}</div>' },
+        { field: 'row', displayName: 'No', width: 50,
+          cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row)+1}}</div>' },
         { field: 'fecharegistro', displayName: 'Fecha de Registro', width: 150 },
         { field: 'tipo', displayName: 'Tipo de Novedad', width: 160 },
         { field: 'numerosolicitud', displayName: 'Número Solicitud', width: 150 },
@@ -31,37 +44,34 @@ angular.module('contractualClienteApp')
         { field: 'observacion', displayName: 'Observación', width: 300 },
         { field: 'motivo', displayName: 'Motivo', width: 250 },
         {
-          field: 'accion', displayName: 'Acción',
-          cellTemplate: '<center>' +
-            '<a class="ver" ng-click="grid.appScope.TiposAvance.load_row(row,\'ver\')" data-toggle="modal" data-target="#modalVer">' +
-            '<i class="fa fa-eye fa-lg  faa-shake animated-hover" aria-hidden="true" data-toggle="tooltip" title="{{\'BTN.VER\' | translate }}"></i></a> ' +
-            '<a class="borrar" ng-click="grid.appScope.TiposAvance.load_row(row,\'delete\');">' +
-            '<i data-toggle="tooltip" title="{{\'BTN.BORRAR\' | translate }}" class="fa fa-trash fa-lg  faa-shake animated-hover" aria-hidden="true"></i></a>' +
-            '</center>',
-          width: 100
+          field: 'accion', displayName: 'Acción', width: 100,
+          cellTemplate:
+            '<center>' +
+              '<a class="ver" ng-click="grid.appScope.TiposAvance && grid.appScope.TiposAvance.load_row ? grid.appScope.TiposAvance.load_row(row,\'ver\') : angular.noop()" data-toggle="modal" data-target="#modalVer">' +
+                '<i class="fa fa-eye fa-lg faa-shake animated-hover" aria-hidden="true" title="{{\'BTN.VER\' | translate}}"></i>' +
+              '</a> ' +
+              '<a class="borrar" ng-click="grid.appScope.TiposAvance && grid.appScope.TiposAvance.load_row ? grid.appScope.TiposAvance.load_row(row,\'delete\') : angular.noop()">' +
+                '<i title="{{\'BTN.BORRAR\' | translate}}" class="fa fa-trash fa-lg faa-shake animated-hover" aria-hidden="true"></i>' +
+              '</a>' +
+            '</center>'
+        },
+        {
+          field: 'anular', displayName: 'Anular', width: 110, enableFiltering: false, enableSorting: false,
+          cellTemplate:
+            '<div class="ui-grid-cell-contents text-center">' +
+              '<button type="button" class="btn btn-xs btn-danger" ' +
+                      'ng-click="grid.appScope.anularFila(row.entity)" ' +
+                      'ng-disabled="row.entity.Activo === false || row.entity.estado === \'ANULADA\'">' +
+                'Anular' +
+              '</button>' +
+            '</div>'
         }
       ],
-      onRegisterApi: function (gridApi) {
-        self.gridApi = gridApi;
-      }
+      onRegisterApi: function (gridApi) { self.gridApi = gridApi; }
     };
 
-    self.gridOptions.data = [{
-      "fecharegistro": "20-06-2017",
-      "tipo": "cesión",
-      "numerosolicitud": "123",
-      "fechasolicitud": "07-06-2017",
-      "observacion": "PRUEBA",
-      "motivo": "PRUEBA"
-    },
-    {
-      "fecharegistro": "20-06-2017",
-      "tipo": "suspensión",
-      "numerosolicitud": "123",
-      "fechasolicitud": "07-06-2017",
-      "observacion": "PRUEBA",
-      "motivo": "PRUEBA"
-    }
-
+    self.gridOptions.data = [
+      { id: 10077, fecharegistro: '20-06-2017', tipo: 'cesión', numerosolicitud: '123', fechasolicitud:'07-06-2017', observacion:'PRUEBA', motivo:'PRUEBA', estado:'CREADA', Activo:true },
+      { id: 10088, fecharegistro: '20-06-2017', tipo: 'suspensión', numerosolicitud: '124', fechasolicitud:'07-06-2017', observacion:'PRUEBA', motivo:'PRUEBA', estado:'CREADA', Activo:true }
     ];
   });
--- app/scripts/services/novedades_mid_service.js
+++ app/scripts/services/novedades_mid_service.js
@@ -13,21 +13,18 @@ angular.module('novedadesMidService', [])
         // Public API here
         var cancelSearch; //defer object
         return {
-            get: function(tabla, params) {
-                cancelSearch = $q.defer();
-                return $http.get(path + tabla + "/" + params, [{ timeout: cancelSearch.promise }, token_service.setting_bearer.headers]);
+            get: function(tabla, params) {cancelSearch = $q.defer();return $http.get(path + tabla + "/" + params,[{ timeout: cancelSearch.promise }, token_service.setting_bearer.headers]);
             },
-            post: function(tabla, elemento) {
-                return $http.post(path + tabla, elemento, token_service.setting_bearer.headers);
+            post: function(tabla, elemento) {return $http.post(path + tabla,elemento,token_service.setting_bearer.headers);
             },
-            put: function(tabla, id, elemento) {
-                return $http.put(path + tabla + "/" + id, elemento, token_service.setting_bearer.headers);
+            put: function(tabla, id, elemento) {return $http.put(path + tabla + "/" + id,elemento,token_service.setting_bearer.headers);
             },
-            delete: function(tabla, id) {
-                return $http.delete(path + tabla + "/" + id, token_service.setting_bearer.headers);
+            patch: function(tabla, id, elemento) {if ($http.patch) {return $http.patch(path + tabla + "/" + id,elemento,token_service.setting_bearer.headers);}
+                return $http({method: 'PATCH',url: path + tabla + "/" + id,data: elemento,headers: token_service.setting_bearer.headers.headers});
             },
-            cancel: function() {
-                return cancelSearch.resolve('search aborted');
+            delete: function(tabla, id) {return $http.delete(path + tabla + "/" + id,token_service.setting_bearer.headers);
+            },
+            cancel: function() {return cancelSearch.resolve('search aborted');
             }
         };
     });
